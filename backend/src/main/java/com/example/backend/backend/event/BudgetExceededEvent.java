3859aeed08414d9cdb762f49a66481bf83d32cbf
Arielparicio28 <arielaparicio100@gmail.com>
2025-03-05 14:18:41 +0100

    Config class BudgetExceededEvent to handle the event when the user is running out of budget

---
+++ backend/src/main/java/com/example/backend/backend/event/BudgetExceededEvent.java
@@ -0,0 +1,20 @@
+package com.example.backend.backend.event;
+
+import lombok.Getter;
+import org.bson.types.ObjectId;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+public class BudgetExceededEvent extends ApplicationEvent {
+
+    private final ObjectId userId;
+    private final Double spent;
+    private final Double limit;
+
+    public BudgetExceededEvent(Object source, ObjectId userId, Double spent, Double limit) {
+        super(source);
+        this.userId = userId;
+        this.spent = spent;
+        this.limit = limit;
+    }
+}
